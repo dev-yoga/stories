7df050a8e63f1d0f080c46cb47ed5413bd7d663e
dev-yoga <spin_shoppe@icloud.com>
2022-02-22 13:05:50 -0800

    Refactor and such

--- src/App.js
+++ src/App.js
@@ -51,13 +51,17 @@ const List = () => (
 
 const Search = () => {
   const handleChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+      <input 
+      id="search" 
+      type="text" 
+      onChange={handleChange} 
+      />
     </div>
   );
 };
