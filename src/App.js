de3aaf58ea497bb54e3e44229fae8212694052c5
dev-yoga <spin_shoppe@icloud.com>
2022-02-21 20:12:23 -0800

    List items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import * as React from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function getTitle(title) {
   return title;
 }
@@ -9,11 +28,19 @@ function App() {
   return (
     <div className="App">
       <h1>
-        Hello {getTitle('React')}
+        My Stories
       </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return <li>{item.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
