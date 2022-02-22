3fe23818f4401007a0d53151639a1a8a4794a866
dev-yoga <spin_shoppe@icloud.com>
2022-02-22 12:57:08 -0800

    Synthetic event

--- src/App.js
+++ src/App.js
@@ -49,11 +49,17 @@ const List = () => (
     </ul>
   );
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+  };
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
+};
 
 export default App;
