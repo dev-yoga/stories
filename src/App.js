a31b855867c4b6e8957481ae9195473391feeaae
dev-yoga <spin_shoppe@icloud.com>
2022-02-22 11:57:45 -0800

    Search component

--- src/App.js
+++ src/App.js
@@ -27,27 +27,11 @@ function App() {
         My Stories
       </h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
       <List />
-
-      {/* <ul>
-        {list.map(function (item) {
-          return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-           </span>
-           <span>{item.author}</span>
-           <span>{item.num_comments}</span>
-           <span>{item.points}</span>
-        </li>
-          );
-        })}
-      </ul> */}
     </div>
   );
 }
@@ -68,6 +52,15 @@ function List() {
         );
       })}
     </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
   );
 }
 export default App;
