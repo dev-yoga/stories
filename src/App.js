e4482801d5c0719513b75f2657b86e10c4c5a1a3
dev-yoga <spin_shoppe@icloud.com>
2022-02-26 22:45:46 -0800

    destructuring

--- src/App.js
+++ src/App.js
@@ -43,17 +43,21 @@ const searchedStories = stories.filter((story)=>
   );
 };
 
-const Search = (props) => (
+const Search = (props) => {
+  const { search, onSearch } = props;
+
+  return (
   <div>
     <label htmlFor="search">Search: </label>
     <input
     id="search" 
     type="text" 
-    value={props.search}
-    onChange={props.onSearch} 
+    value={search}
+    onChange={onSearch}
     />
   </div>
-);
+  );
+};
 
 const List = (props) => (
   <ul>
