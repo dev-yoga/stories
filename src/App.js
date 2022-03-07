156aef9d623695bf9ee0989f132fe3d89c318cf6
dev-yoga <spin_shoppe@icloud.com>
2022-03-06 18:52:35 -0800

    remove continued

--- src/App.js
+++ src/App.js
@@ -108,23 +108,32 @@ const InputWithLabel = ({
   );
 };
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item 
+      key={item.objectID} 
+      item={item}
+      onRemoveItem={onRemoveItem}
+     />
     ))}
   </ul>
 );
 
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-);
+const Item = ({ item, onRemoveItem }) => {
+  const handleRemoveItem = () => {
+    onRemoveItem(item)
+  };
+  return (
+    <li>
+      <span>
+       <a href={item.url}>{item.title}</a>
+     </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+    );
+  };
 
 export default App;
