4b39bf865416a85a51bea2c0e164e593faaf0180
dev-yoga <spin_shoppe@icloud.com>
2022-03-13 22:08:16 -0700

    refactor to a switch statement

--- src/App.js
+++ src/App.js
@@ -40,10 +40,16 @@ const useSemiPersistentState = (key, initialState) => {
 };
     
 const storiesReducer = (state, action) => {
-  if (action.type === 'SET_STORIES') {
-    return action.payload;
-  } else {
-    throw new Error();
+//refactor to switch statement
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectOD !== story.objectID
+      );
+      default:
+        throw new Error();
   }
 };
 
